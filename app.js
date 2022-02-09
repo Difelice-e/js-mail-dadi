b894077f1c9f01ae3c6bfc99683a250bb5429d40
Difelice-e <difelice.emanuele@gmail.com>
2022-02-09 15:52:51 +0100

    struttura js

---
+++ app.js
@@ -0,0 +1,17 @@
+// CONTROLLO EMAIL 
+
+// creazione di un array con le email valide 
+// input dell'utente della propria mail
+// controllo se la mail è presente in lista
+// stampo messaggio di esito positivo o negativo
+
+
+
+
+
+// GIOCO DEI DADI
+
+// generazione numero random 1-6 per l'utente
+// generazione numero random 1-6 per il pc 
+// controllo i valori per decidere il più alto 
+// stampo messaggio di esito (vittoria, sconfitta o pareggio)
