7aeef0b0a5af3a4b1c0a9ce5cd77f51ed017074a
Difelice-e <difelice.emanuele@gmail.com>
2022-02-09 16:23:10 +0100

    prima parte completata

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 // CONTROLLO EMAIL 
 
 // creazione di un array con le email valide 
-// input dell'utente della propria mail
-// controllo se la mail è presente in lista
-// stampo messaggio di esito positivo o negativo
+const emailRegistrate = ["boolean@gmail.com", "official.booleanògmail.com", "example@gmail.com"];
 
+// input dell'utente della propria mail e creazione della variabile
+const userMail = document.getElementById('email-utente');
 
-
+document.getElementById('submit-button').addEventListener("click",   // aggiunto evento di click sul bottone che starta il ciclo
+    function () {
+        for (let i = 0; i < emailRegistrate.length; i++) {
+            if (emailRegistrate[i] === userMail.value) {   // controllo se la mail è presente in lista
+                alert("La tua email è valida, puoi accedere al sito!")   // stampo messaggio di esito positivo
+                break
+            } else {
+                alert("Ci dispiace, la tua email non è registrata!")   // stampo messaggio di esito negativo
+                break
+            }
+        }
+    }
+)
 
 
 // GIOCO DEI DADI
 
 // generazione numero random 1-6 per l'utente
-// generazione numero random 1-6 per il pc 
-// controllo i valori per decidere il più alto 
+// generazione numero random 1-6 per il pc
+// controllo i valori per decidere il più alto
 // stampo messaggio di esito (vittoria, sconfitta o pareggio)
